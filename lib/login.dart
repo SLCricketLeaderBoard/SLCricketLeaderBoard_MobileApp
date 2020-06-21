080b8cd5a48c747d3302b22a7f5e2168cc54c4fb
Ruwan <ruwannawarathna96@gmail.com>
2020-06-21 20:13:57 +0530

    email validations added

--- lib/login.dart
+++ lib/login.dart
@@ -28,9 +28,14 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   String emailValidator(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
     if (value.isEmpty) {
       return 'Enter your email address';
-    } else {
+    } else if(!regex.hasMatch(value)) {
+      return 'Email format is invalid';
+    } else{
       return null;
     }
   }
