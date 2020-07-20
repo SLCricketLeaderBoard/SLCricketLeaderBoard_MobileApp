fdad719135a270d332a9db9d3d1f803f8f711f76
Shalitha Jayamal <47962458+ShalithaJayamal@users.noreply.github.com>
2020-07-20 23:55:26 +0530

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,6 @@
 // import 'dart:html';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'home.dart';
 import 'package:toast/toast.dart';
 
