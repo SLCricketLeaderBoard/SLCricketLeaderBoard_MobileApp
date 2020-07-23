8adf5f8800dd00a1a98aacf4ecddb9144a559647
ssss <shalithajayamal96@gmail.com>
2020-07-24 00:33:37 +0530

    update functiond modified

---
+++ lib/note.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+
+class NotePage extends StatelessWidget {
+  var value;
+  NotePage({Key key, this.value}) : super(key: key);
+
+  static String tag = 'calculator-page';
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
