b7393cec7c1decc25ef8a1db14abc6a91201632e
Ruwan <ruwannawarathna96@gmail.com>
2020-08-03 00:27:51 +0530

    edit chat component

--- lib/chat.dart
+++ lib/chat.dart
@@ -2,25 +2,80 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/rendering.dart';
 import './Drawer/drawer.dart';
+import './Widgets/ReceiveMessageWidget.dart';
+import './Widgets/SendMessageWidget.dart';
 
-class Chat extends StatelessWidget {
+class Chat extends StatefulWidget {
   var value;
   Chat({Key key, this.value}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final alucard = Hero(
-      tag: 'hero',
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: CircleAvatar(
-          radius: 72.0,
-          backgroundColor: Colors.transparent,
-          backgroundImage: NetworkImage(this.value["profileImage"]),
-        ),
-      ),
-    );
+  _ChatViewState createState() => _ChatViewState();
+}
 
+class _ChatViewState extends State<Chat> {
+  TextEditingController _text = new TextEditingController();
+  ScrollController _scrollController = ScrollController();
+  var childList = <Widget>[];
+
+  @override
+  void initState() {
+    super.initState();
+    childList.add(Align(
+        alignment: Alignment(0, 0),
+        child: Container(
+          margin: const EdgeInsets.only(top: 5.0),
+          height: 25,
+          width: 50,
+          decoration: BoxDecoration(
+              color: Colors.black12,
+              borderRadius: BorderRadius.all(
+                Radius.circular(8.0),
+              )),
+          child: Center(
+              child: Text(
+            "Today",
+            style: TextStyle(fontSize: 11),
+          )),
+        )));
+    childList.add(Align(
+      alignment: Alignment(1, 0),
+      child: SendedMessageWidget(
+        content: 'Hello',
+        time: '21:36 PM',
+      ),
+    ));
+    childList.add(Align(
+      alignment: Alignment(1, 0),
+      child: SendedMessageWidget(
+        content: 'How are you? What are you doing?',
+        time: '21:36 PM',
+      ),
+    ));
+    childList.add(Align(
+      alignment: Alignment(-1, 0),
+      child: ReceivedMessageWidget(
+        content: 'Hello, Mohammad.I am fine. How are you?',
+        time: '22:40 PM',
+      ),
+    ));
+    childList.add(Align(
+      alignment: Alignment(1, 0),
+      child: SendedMessageWidget(
+        content:
+            'I am good. Can you do something for me? I need your help my bro.',
+        time: '22:40 PM',
+      ),
+    ));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final bodyDetails = Padding(
       padding: EdgeInsets.all(8.0),
       child: Text(
@@ -39,12 +94,12 @@ class Chat extends StatelessWidget {
 
     return Scaffold(
       appBar: new AppBar(
-        title: new Text("Chat"),
+        title: new Text("Chats"),
         backgroundColor: Colors.grey[500],
       ),
       backgroundColor: Colors.grey[800],
       body: body,
-      drawer: AppDrawer(value: this.value),
+      drawer: AppDrawer(value: widget.value),
     );
   }
 }
